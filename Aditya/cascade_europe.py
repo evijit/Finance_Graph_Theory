47905179f2fcda008d7e1b27dab7eb143ef1f9b3
adityachourasiya <32713742+adityachourasiya@users.noreply.github.com>
2018-08-15 23:05:17 +0530

    Updated .py with country failing according to wave

--- Aditya/cascade_europe.py
+++ Aditya/cascade_europe.py
@@ -14,13 +14,11 @@ gdp_08 = np.array(gdp_08)
 gdp_11 = np.array(gdp_11)
 
 normalizing_value = gdp_11[4]
-
 gdp_08_normalized = [x / normalizing_value for x in gdp_08]
 gdp_11_normalized = [x / normalizing_value for x in gdp_11]
 fraction_gdp_loss = (gdp_08-gdp_11)/gdp_08
 
 c = 0.33
-n = len(header)
 column_sum = np.array(sum(debt_matrix))
 inverse_column_sum = 1 / column_sum
 
@@ -33,33 +31,28 @@ C_hat = np.diag(C_hat)
 
 A_matrix = np.dot(C_hat, np.linalg.inv((np.diag(np.ones(len(C_matrix))) - C_matrix)))
 
-print(np.around(A_matrix,decimals=2))
-
-theta=.9
-
 p = np.matrix(gdp_11_normalized).T
 
+theta=.95
+
 gdp_08_normalized = np.matrix(gdp_08_normalized).T
+
 v_threshold = theta*(np.dot(A_matrix, gdp_08_normalized))
 
 pcurrent = p
+
+n = 6
 all_failure_indicator = (np.full((1, n), False)).astype(int)
-while True:
-    new_failure_indicator = np.array((np.dot(A_matrix, pcurrent).T < v_threshold.T).astype(int)) * ((all_failure_indicator == 0)).astype(int)
-    print (new_failure_indicator,all_failure_indicator)
-    all_failure_indicator = all_failure_indicator | new_failure_indicator
-    print (all_failure_indicator)
-    pcurrent = pcurrent - new_failure_indicator*v_threshold/2
+wave = 0
+while (wave == 0) or (len(new_failed_names) != 0):
+    wave = wave + 1
+    new_failure_indicator = (np.array((np.dot(A_matrix, pcurrent).T < v_threshold.T).astype(int)) * ((all_failure_indicator == 0))).T
+    all_failure_indicator = all_failure_indicator | new_failure_indicator.T
+    pcurrent = pcurrent - np.multiply(new_failure_indicator, v_threshold/2)
     new_failed_countries = np.where(all_failure_indicator[0] == 1)[0]
-    new_failed_names = header[new_failed_countries[0]]
-    new_failed_names = [ header[i] for i in new_failed_countries ]
-
-    print("Theta: ",theta)
-    print(new_failed_names)
-    print(new_failed_countries)
-    print('\n')
-    if (len(new_failed_countries)==len(header)):
-        print('All countries failed for Theta: ',theta)
-        break
-    theta+=0.001
-
+    a = np.where(new_failure_indicator.T[0] == 1)
+    new_failed_names = []
+    for x in list(a[0]):
+        new_failed_names.append(header[x])
+    if (len(new_failed_names) != 0):
+        print('Wave', wave, 'failed countries are', new_failed_names)
