2e204827edc0404c3ed92cd431dcf7bcedbeab64
Avijit Ghosh <avijitg22@gmail.com>
2018-08-15 10:19:52 +0530

    Add csv and fixed code

--- Aditya/cascade_europe.py
+++ Aditya/cascade_europe.py
@@ -32,7 +32,9 @@ C_hat = np.diag(C_hat)
 
 A_matrix = np.dot(C_hat, np.linalg.inv((np.diag(np.ones(len(C_matrix))) - C_matrix)))
 
-theta=.95
+print(np.around(A_matrix,decimals=2))
+
+theta=.9
 
 p = np.matrix(gdp_11_normalized).T
 
@@ -41,13 +43,20 @@ v_threshold = theta*(np.dot(A_matrix, gdp_08_normalized))
 
 pcurrent = p
 all_failure_indicator = (np.full((1, n), False)).astype(int)
-for i in range(3):
+while True:
     new_failure_indicator = np.array((np.dot(A_matrix, pcurrent).T < v_threshold.T).astype(int)) * ((all_failure_indicator == 0)).astype(int)
     all_failure_indicator = all_failure_indicator | new_failure_indicator
     pcurrent = pcurrent - new_failure_indicator*v_threshold/2
     new_failed_countries = np.where(all_failure_indicator[0] == 1)[0]
     new_failed_names = header[new_failed_countries[0]]
-    
+    new_failed_names = [ header[i] for i in new_failed_countries ]
+
+    print("Theta: ",theta)
     print(new_failed_names)
     print(new_failed_countries)
     print('\n')
+    if (len(new_failed_countries)==len(header)):
+        print('All countries failed for Theta: ',theta)
+        break
+    theta+=0.001
+
