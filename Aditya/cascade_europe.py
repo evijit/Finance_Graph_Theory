81a567d0082cec6e52afbe97368f063f024cb651
Aditya Chourasiya <adityachourasiya142@gmail.com>
2018-08-17 02:01:45 +0530

    Updated code for Casecade with plotting graph after every wave and respective images for theta 0.935 and 0.94

--- Aditya/cascade_europe.py
+++ Aditya/cascade_europe.py
@@ -1,5 +1,11 @@
-import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
 import pandas as pd
+import numpy as np
+import networkx as nx
+plt.rcParams['figure.figsize'] = [15, 15]
+
+Graph_data = nx.MultiDiGraph()
 
 header = ['France','Germany','Greece','Italy','Portugal','Spain']
 debt_matrix = pd.read_csv('country.csv', names = header)
@@ -31,9 +37,12 @@ C_hat = np.diag(C_hat)
 
 A_matrix = np.dot(C_hat, np.linalg.inv((np.diag(np.ones(len(C_matrix))) - C_matrix)))
 
+matrix_data = pd.DataFrame(A_matrix, index = header)
+matrix_data.columns = header
+
 p = np.matrix(gdp_11_normalized).T
 
-theta=.95
+theta=.94
 
 gdp_08_normalized = np.matrix(gdp_08_normalized).T
 
@@ -43,16 +52,51 @@ pcurrent = p
 
 n = 6
 all_failure_indicator = (np.full((1, n), False)).astype(int)
+
+node_size = list(np.array(np.dot(A_matrix, pcurrent)).T[0])
+
+node_sizes = {}
+for i in range(len(node_size)):
+    node_sizes[header[i]] = node_size[i]
+
+node_sizes = {}
+for i in range(len(node_size)):
+    node_sizes[header[i]] = node_size[i]
+
+for country in header:
+    Graph_data.add_node(country)
+
+for country in header:
+    for country2 in header:
+        if country != country2:
+            Graph_data.add_weighted_edges_from([(country, country2, matrix_data[country][country2])])
+
 wave = 0
 while (wave == 0) or (len(new_failed_names) != 0):
     wave = wave + 1
+    nx.draw(Graph_data, with_labels = True,  node_size=[v * 1000 for v in node_sizes.values()])
+    plt.savefig('myfig' + str(wave) + '.png')
+    plt.figure()
+    
     new_failure_indicator = (np.array((np.dot(A_matrix, pcurrent).T < v_threshold.T).astype(int)) * ((all_failure_indicator == 0))).T
     all_failure_indicator = all_failure_indicator | new_failure_indicator.T
     pcurrent = pcurrent - np.multiply(new_failure_indicator, v_threshold/2)
     new_failed_countries = np.where(all_failure_indicator[0] == 1)[0]
     a = np.where(new_failure_indicator.T[0] == 1)
+    
+    node_size = list(np.array(np.dot(A_matrix, pcurrent)).T[0])
+
+    node_sizes = {}
+    for i in range(len(node_size)):
+        node_sizes[header[i]] = node_size[i]
+        
+    for j in new_failed_countries:
+        del node_sizes[header[j]]
+    
     new_failed_names = []
     for x in list(a[0]):
+        Graph_data.remove_node(header[x])
         new_failed_names.append(header[x])
+        
     if (len(new_failed_names) != 0):
         print('Wave', wave, 'failed countries are', new_failed_names)
