300f99d710a35e1bbc96878666ea9169769e6ccb
Lakshay Bansal <lakshayb5@gmail.com>
2018-08-16 13:10:02 -0400

    Updated Code and fixed wave failure error

--- Lakshay/Europe Cascade/EuropeCascade.py
+++ Lakshay/Europe Cascade/EuropeCascade.py
@@ -32,35 +32,32 @@ C_hat = (np.array(sum((np.diag(np.ones(len(C_matrix))) - C_matrix))))[0]
 C_hat = np.diag(C_hat)
 
 A_matrix = np.dot(C_hat, np.linalg.inv((np.diag(np.ones(len(C_matrix))) - C_matrix)))
-print(np.around(A_matrix,decimals=2))
+#print(np.around(A_matrix,decimals=2))
 
 p = np.matrix(gdp_11_normalized).T
 gdp_08_normalized = np.matrix(gdp_08_normalized).T
 
 theta = 0.9
-v_threshold = theta * np.dot(A_matrix, gdp_08_normalized)
+v_threshold = theta * (np.dot(A_matrix, gdp_08_normalized))
 pcurrent = p
 wave = 0
 all_failure_indicator = np.zeros(6, dtype = int)
-new_failed_countries = np.where(all_failure_indicator[0] == 1)[0]
 
-#def failure_function(theta):
-while (wave == 0 or np.all(new_failed_countries != 0)):
+while (wave == 0) or (len(new_failed_names) != 0):
     wave = wave + 1
-    new_failure_indicator = np.array((np.dot(A_matrix, pcurrent).T < v_threshold.T).astype(int)) * (all_failure_indicator == 0).astype(int)
-    #print (new_failure_indicator , all_failure_indicator)
-    all_failure_indicator = all_failure_indicator | new_failure_indicator
-    pcurrent = pcurrent - new_failure_indicator*(v_threshold/2)
-    new_failed_countries = np.where(new_failure_indicator[0] == 1)[0]
-    all_failed_countries = np.where(all_failure_indicator[0] == 1)[0]
-    #new_failed_names = countries[new_failed_countries[0]]
-    new_failed_names = [ countries[i] for i in new_failed_countries ]
-    all_failed_names = [ countries[i] for i in all_failed_countries ]
+    new_failure_indicator = (np.array((np.dot(A_matrix, pcurrent).T < v_threshold.T).astype(int)) * (all_failure_indicator == 0)).T
+    all_failure_indicator = all_failure_indicator | new_failure_indicator.T
+    pcurrent = pcurrent - np.multiply(new_failure_indicator, v_threshold/2)
 
-    print("Total countries failed in wave:",wave)
-    print(all_failed_names)
-    #print(new_failed_countries)
-    #print('\n')
-    if (len(all_failed_countries)==len(countries)):
-        print('All countries failed for Theta:',theta)
-        break
+    new_failed_countries = np.where(new_failure_indicator.T[0] == 1)[0]
+   # new_failed_countries = np.where(all_failure_indicator[0] == 1)[0]    #To print all failed countries in a wave
+    new_failed_names = []
+    new_failed_names = [ countries[i] for i in new_failed_countries]
+    #all_failed_names = [ countries[i] for i in all_failed_countries ]
+    if (len(new_failed_names) != 0):
+      print("Countries failed in wave:",wave)
+      print(new_failed_names)
+
+    if (len(new_failed_countries) == n):
+      break
+   
